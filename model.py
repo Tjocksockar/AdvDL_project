ed8516661bf548e9c5be94ee8268c5f0a5748582
Xuechen Zuo <grisen@Xuechens-Mini.lan>
2019-09-26 15:37:04 +0200

    imports added

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
-import tensorflow
+import tensorflow as tf 
+
+from keras.applications import ResNet50
+from keras.applications import ResNet101
+from keras.applications import ResNet152
+
+model = ResNet152(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+model.summary()
